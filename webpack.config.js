16097734d8e564ecc47e6dbaccda5036d43dd410
tacticalParmesan <edoardoferreri10@gmail.com>
2024-09-30 21:17:35 +0200

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { assert } = require("console");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/main.js",
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+    port: 8082,
+    liveReload: true,
+    hot: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Weather App | Odin Project",
+      template: "./src/index.html",
+    }),
+  ],
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+
+  optimization: {
+    runtimeChunk: "single",
+  },
+  target: "web",
+  watchOptions: {
+    ignored: /node_modules/, // Exclude node_modules directory from watching
+    poll: true, // Check for changes every 1000 milliseconds (1 second)
+  },
+};
